d129ba908f24b73f997d71a38d0bca8f0163b9e0
Mathias Johansson <mathias9807@gmail.com>
2025-04-13 11:10:01 +0200

    Move dist folder to ./docs

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  outputDir: './docs',
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/sheet-music/'
+    ? '/sheet-music-practice/'
     : '/',
 })
